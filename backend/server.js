32a1fa46934441325929c8736983b4e82a637cb3
mhassan-pro <mhassan.pro0@gmail.com>
2026-02-17 17:30:23 +0500

    update package.json and server.js: set module type and add API endpoint for notes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ import express from "express";
 // Create a server called "app" using Express
 const app = express();
 
+// When someone visits "/api/notes", send them a message with the notes count
+app.get("/api/notes", (req,res)=>{
+    res.send("You got 6 notes..")
+});
 
 // A method used to listen incomming requests on ports 5000.
 app.listen(5000,()=>{
